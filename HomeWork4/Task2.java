c803608cbd1a9ed952dd224114a1e8d2fc80465a
MikhailChizhev <mchigev@gmail.com>
2023-04-06 14:58:21 +0300

    HomeWork4

---
+++ HomeWork4/Task2.java
@@ -0,0 +1,27 @@
+//2. Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+
+
+package HomeWork4;
+import java.util.LinkedList;
+
+public class Task2 {
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            ll.add(getRandomNumber());
+        }
+        System.out.println(ll);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(ll.getLast() + ", ");
+            ll.removeLast();
+
+        }
+    }
+
+    public static int getRandomNumber() { 
+        double x = (Math.random() * 50);
+        int num = (int) x;
+        return num;
+    }
+}
