f0924fdf6f56305202386950e14d792f386b34a7
MikhailChizhev <mchigev@gmail.com>
2023-04-12 20:51:47 +0300

    6

---
+++ HomeWork6/Notebook.java
@@ -0,0 +1,21 @@
+package HomeWork6;
+
+public class Notebook {
+
+    public Notebook(String string, String string2, String string3, double d, String string4, int i, int j,
+            String string5) {
+    }
+
+    public String getColor() {
+        return null;
+    }
+
+    public char[] printInfo() {
+        return null;
+    }
+
+    public String getBrand() {
+        return null;
+    }
+
+}
